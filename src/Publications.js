f1a32908b5f98e351b851b576e72cd71f9fc3dfd
Jared Coleman <jaredraycoleman@gmail.com>
2020-09-01 07:43:09 -0700

    Fixes issues when publications include inproceedings

--- src/Publications.js
+++ src/Publications.js
@@ -4,6 +4,8 @@ import {Row, Col, ListGroup } from 'react-bootstrap';
 import { xml2js } from 'xml-js';
 import axios from 'axios';
 
+const DBLP_USER = "https://dblp.org/pid/268/5846.xml";
+
 class Publications extends React.Component {
     constructor(props) {
         super(props);
@@ -14,9 +16,17 @@ class Publications extends React.Component {
     }
 
     componentDidMount() {
-        axios.get("https://dblp.org/pid/268/5846.xml").then(res => {
-            let xml = xml2js(res.data, { compact: true });
-            let pubs = xml.dblpperson.r.article ? [xml.dblpperson.r.article] : xml.dblpperson.r;
+        axios.get(DBLP_USER).then(res => {
+            const xml = xml2js(res.data, { compact: true });
+
+            let pubs = [];
+            if (xml.dblpperson.r.article) pubs.push(xml.dblpperson.r.article);
+            if (xml.dblpperson.r.inproceedings) pubs.push(xml.dblpperson.r.inproceedings);
+            (Array.isArray(xml.dblpperson.r) ? xml.dblpperson.r : []).forEach(pub => {
+                if (pub.article) pubs.push(pub.article);
+                if (pub.inproceedings) pubs.push(pub.inproceedings);
+            })
+            
             this.setState({ pubs });
         }).catch(err => {
             console.error(err);
@@ -26,14 +36,16 @@ class Publications extends React.Component {
     render() {
         const { pubs } = this.state;
         const pubitems = pubs.map(pub => {
-            console.log(pub);
-            const authors = pub.author.map(author => {
+            const authors = (Array.isArray(pub.author) ? pub.author : [pub.author]).map(author => {
                 return <a href={`https://dblp.org/pid/${author._attributes.pid}`} style={{color: "gray"}}>{author._text}</a>
             }).reduce((prev, curr) => [prev, ", ", curr]);
+            const publisher = pub.journal ? pub.journal._text : (pub.booktitle ? pub.booktitle._text : null);
             return (
-                <ListGroup.Item>
+                <ListGroup.Item key={pub.ee._text}>
                     <a href={pub.ee._text} style={{color: "black"}}><b>{pub.title._text}</b></a>
                     <br />
+                    {publisher}
+                    {publisher ? <br /> : null}
                     {authors}
                 </ListGroup.Item>
             );
@@ -42,7 +54,7 @@ class Publications extends React.Component {
         return (
             <Row>
                 <Col>
-                    <h2 className="text-center">Publications</h2>
+                    <h1 className="text-center">Publications</h1>
                     <ListGroup>
                         {pubitems}
                     </ListGroup>
