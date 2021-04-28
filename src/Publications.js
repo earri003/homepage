d505660f1c7c1c392ba6cbbae82ed686ce8b29f6
Jared Coleman <jaredraycoleman@gmail.com>
2021-04-28 14:47:01 -0700

    Updates publications

--- src/Publications.js
+++ src/Publications.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import {Row, Col, ListGroup } from 'react-bootstrap';
 
+const websites = {
+    "Jared Coleman": "https://jaredraycoleman.com",
+    "Evangelos Kranakis": "http://people.scs.carleton.ca/~kranakis/",
+    "Oscar Morales-Ponce": "http://web.csulb.edu/~omorales/",
+    "Jorge Urrutia": "https://www.matem.unam.mx/~urrutia/",
+    "Birgit Vogtenhuber": "http://www.ist.tugraz.at/staff/vogtenhuber/"
+}
+
+const pubs = [
+    {
+        "title": "Minimizing The Maximum Distance Traveled To Form Patterns With Systems of Mobile Robots",
+        "venue": "In proceedings CCCG 2020, 32nd Canadian Conference on Computational Geometry, August 5-7, 2020",
+        "authors": ["Jared Coleman", "Evangelos Kranakis", "Oscar Morales-Ponce", "Jorge Urrutia", "Birgit Vogtenhuber"],
+        "link": "https://arxiv.org/pdf/2006.15664.pdf"
+    }
+]
+
 function Publications () {
+    const pubitems = pubs.map(pub => {
+        const authors = pub.authors.map(author => {
+            return <a href={websites[author]} style={{color: "gray"}}>{author}</a>
+        }).reduce((prev, curr) => [prev, ", ", curr]);
+        return (
+            <ListGroup.Item key={pub.link}>
+                <a href={pub.link} style={{color: "black"}}><b>{pub.title}</b></a>
+                <br />
+                {pub.venue}
+                <br />
+                {authors}
+            </ListGroup.Item>
+        );
+    });
+
     return (
         <Row>
             <Col>
                 <h1 className="text-center">Publications</h1>
                 <ListGroup>
-                    <ListGroup.Item key="robot-pattern-formation">
-                        <a href="https://arxiv.org/pdf/2006.15664" style={{color: "black"}}><b>Minimizing The Maximum Distance Traveled To Form Patterns With Systems of Mobile Robots</b></a>
-                        <br />
-                        In proceedings CCCG 2020, 32nd Canadian Conference on Computational Geometry, August 5-7, 2020
-                        <br />
-                        <a href="https://jaredraycoleman.com">J. Coleman</a>
-                        <a href="http://people.scs.carleton.ca/~kranakis/">E. Kranakis</a>
-                        <a href="http://web.csulb.edu/~omorales/">O. Morales-Ponce</a>
-                        <a href="https://www.matem.unam.mx/~urrutia/">J. Urrutia</a>
-                        <a href="http://www.ist.tugraz.at/staff/vogtenhuber/">B. Vogtenhuber</a>
-                    </ListGroup.Item>
+                    {pubitems}
                 </ListGroup>
             </Col>
         </Row>
