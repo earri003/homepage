f2e6e84ee62eb54e6c55d33b6fdaeab415583822
Jared Coleman <jaredraycoleman@gmail.com>
2021-04-28 14:31:38 -0700

    updates publications

--- src/Publications.js
+++ src/Publications.js
@@ -7,8 +7,8 @@ function Publications () {
             <Col>
                 <h1 className="text-center">Publications</h1>
                 <ListGroup>
-                    <ListGroup.Item key={pub.ee._text}>
-                        <a href={pub.ee._text} style={{color: "black"}}><b>Minimizing The Maximum Distance Traveled To Form Patterns With Systems of Mobile Robots</b></a>
+                    <ListGroup.Item key="robot-pattern-formation">
+                        <a href="https://arxiv.org/pdf/2006.15664" style={{color: "black"}}><b>Minimizing The Maximum Distance Traveled To Form Patterns With Systems of Mobile Robots</b></a>
                         <br />
                         In proceedings CCCG 2020, 32nd Canadian Conference on Computational Geometry, August 5-7, 2020
                         <br />
