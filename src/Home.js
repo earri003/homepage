2f5be609464d670807216b817ae5b321013b9f8d
Jared Ray Coleman <jaredraycoleman@gmail.com>
2021-10-07 22:12:59 +0000

    Updates home page

--- src/Home.js
+++ src/Home.js
@@ -12,14 +12,14 @@ function Home() {
             <h1 className="text-center">Jared Coleman</h1>
             <p className="mt-3 text-center">
                 I am a <b>Ph.D. student</b> at <a href="https://usc.edu">University of Southern California</a> advised by <a href="http://ceng.usc.edu/~bkrishna/">Bhaskar Krishnamachari</a>.
-                I have <b>Master's and Bachelor's degrees</b> in <b>Computer Science</b> from <a href="https://csulb.edu">California State University, Long Beach</a>.
-                I am a <b>Member of the Technical Staff</b> at the Federally Funded Research and Development Center, <a href="https://aerospace.org">Aerospace Corporation</a>.
+                I have Master's and Bachelor's degrees in <b>Computer Science</b> from <a href="https://csulb.edu">California State University, Long Beach</a>.
+                I am also a Member of the Technical Staff at the Federally Funded Research and Development Center, <a href="https://aerospace.org">Aerospace Corporation</a>.
             </p> 
     
             <h2 className="text-center">Research Interests</h2>
             <p className="text-center">
                 I have a broad interest in computer science and mathematics. 
-                In particular, my research interests include computational geometry, discrete mathematics, graph theory, distributed computing, blockchain, and <b>mobile robotic networks</b>.
+                In particular, my research interests include blockchain, algorithms, mobile robotic networks, discrete mathematics, graph theory, and distributed computing.
             </p>
         </Col>
       </Row>
